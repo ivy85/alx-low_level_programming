d30ecb9e8b27c00f4b5361d1b9be0de9ba1e1091
ivy85 <ivykerubo2021@gmail.com>
2022-12-05 13:22:32 -0800

    Project 0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a textfile and prints the letters.
+ * @filename: the filename.
+ * @letters: nummbers of the letters printed.
+ *
+ * Return: numbers of letters printed.
+ *	if it fails: 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
